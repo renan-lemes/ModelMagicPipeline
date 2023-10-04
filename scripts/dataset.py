1e07e4549fdaf8f7c9ba42413df086f8ce9a35a8
renanll <renan_ll@hotmail.com>
2023-10-03 21:23:39 -0300

    Arrumamos os dados do nosso dataframe e modelemos para test e train

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,2 +1,24 @@
-import pandas as pd
-import numpy as np
+## Carregar dados
+def LoadData():
+    from pandas import read_csv
+
+    train = read_csv("D:\\Projeto_\\ModelMagicPipeline\\data\\train.csv")
+    test = read_csv("D:\\Projeto_\\ModelMagicPipeline\\data\\test.csv")
+    return train, test
+
+
+## Class para poder arrumar os dados de treino e teste dos modelos
+class DataStruct:
+    def __init__(self, train, test):
+        self.test = test
+        self.train = train
+
+    def data_cat__num(self):
+        self.catecoric_features = [
+            col for col in train.columns if self.train[col].dtype.name == "object"
+        ]
+        self.numeric_features = [
+            col
+            for col in self.train.columns
+            if col not in catecoric_features and col != "Survived"
+        ]
